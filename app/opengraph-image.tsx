e3a8c1b4695e0fa4596df024e4fd22d3b41abf08
Matt Hall <25968932+matthall00@users.noreply.github.com>
2025-09-15 22:49:18 -0400

    fix: set runtime to edge in opengraph image generation

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/og";
+export const runtime = "edge";
 
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
@@ -51,4 +52,3 @@ export default function OpengraphImage() {
     { ...size }
   );
 }
-
